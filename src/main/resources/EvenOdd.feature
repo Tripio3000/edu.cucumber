38f5b36c8dcc100e2941de60e7b580a46b908286
Alex <mr.alex-kon@yandex.ru>
2021-01-11 17:14:39 +0300

    first commit

---
+++ src/main/resources/EvenOdd.feature
@@ -0,0 +1,6 @@
+Feature: EvenOdd feature
+
+  Scenario: Run programm to get Even
+    Given Create a EvenOdd programm
+    When Run with the 6
+    Then The result is Even
---
+++ src/test/java/EvenOddStepdefs.java
@@ -0,0 +1,13 @@
+public class EvenOddStepdefs {
+    @Given("^Create a EvenOdd programm$")
+    public void createAEvenOddProgramm() {
+    }
+
+    @cucumber.api.java.en.When("^Run with the (\\d+)$")
+    public void runWithThe(int arg0) {
+    }
+
+    @cucumber.api.java.en.Then("^The result is Even$")
+    public void theResultIsEven() {
+    }
+}
